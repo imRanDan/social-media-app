0670cf4e07610af67b2a88264024ea348bf8b916
Danyal Imran <dan.imran97@gmail.com>
2024-08-14 23:48:08 -0400

    working on sidebar

--- src/Layouts/PageLayout/PageLayout.jsx
+++ src/Layouts/PageLayout/PageLayout.jsx
@@ -1,12 +1,20 @@
 import { Flex, Box } from '@chakra-ui/react'
+import Sidebar from '../../components/Sidebar/Sidebar'
+import { useLocation } from 'react-router-dom'
 
 const PageLayout = ({children}) => {
+  const {pathname} = useLocation()
+
   return (
     <Flex>
         {/* Sidebar, left-side */}
-
+        {pathname !== '/auth' ? (
+          <Box w={{base:"70px",md:"240px"}}>
+            <Sidebar />
+          </Box>
+          ) : null}
         {/* Page Content, right-side */}
-        <Box>
+        <Box flex={1} w={{base:"calc(100% - 70px)",md:"calc(100% - 240px" }}>
             {children}
         </Box>
     </Flex>
---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,20 @@
+import { Box, Flex, Link } from "@chakra-ui/react";
+import { Link as RouterLink } from "react-router-dom";
+import {InstagramLogo} from "../../assets/contants";
+
+const Sidebar = () => {
+  return (
+    <Box height={"100"} borderRight={"1px solid"} borderColor={"whiteAlpha.300"} py={8} position={"sticky"} top={0} left={0} px={{base:2,md:4}}>
+        <Flex direction={"column"} gap={10} w="full" height={"full"}>
+            <Link to={"/"} as={RouterLink} pl={2} display={{base:"none",md:"block"}} cursor={"pointer"}>
+                <InstagramLogo />
+            </Link>
+            <Link to={"/"} as={RouterLink} pl={2} display={{base:"block",md:"none"}} borderRadius={6} _hover={{bg:"whiteAlpha.200"}}w={10} cursor="pointer">
+                {/* <InstagramMobileLogo /> */}
+            </Link>
+        </Flex>
+    </Box>
+  )
+}
+
+export default Sidebar
