5bad08c678d8ada0b73873e7830465d2f9b5b60e
Danyal Imran <dan.imran97@gmail.com>
2024-08-17 15:03:49 -0400

    added in skeleton loader

--- src/components/FeedPosts/FeedPosts.jsx
+++ src/components/FeedPosts/FeedPosts.jsx
@@ -1,14 +1,42 @@
-import { Container } from "@chakra-ui/react"
+import { Container, Box, VStack, Flex, SkeletonCircle, Skeleton } from "@chakra-ui/react"
 import FeedPost from "./FeedPost"
+import { useEffect, useState } from 'react';
 
 
 const FeedPosts = () => {
+    const[isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setIsLoading(false)
+        }, 2000)
+    },[])
+
   return (
     <Container maxW={"container.sm"} py={10} px={2}>
-        <FeedPost img='/img1.png' username='ye' avatar='/img1.png' />
-        <FeedPost img='/img2.png' username='bobby' avatar='/img2.png' />
-        <FeedPost img='/img3.png' username='kelly' avatar='/img3.png' />
-        <FeedPost img='/img4.png' username='johndoe' avatar='/img4.png' />
+        {isLoading && [0,1,2,3].map((_,idx) => (
+         <VStack key={idx} gap={4} alignItems={"flex-start"} mb={10}>
+            <Flex gap={2}>
+                <SkeletonCircle size='10' />
+                <VStack gap={2} alignItems={"flex-start"}>
+                  <Skeleton height='10px' w={"200px"} />
+                  <Skeleton height='10px' w={"200px"} />
+                </VStack>
+            </Flex>
+            <Skeleton w={"full"}>
+                <Box h={"500px"}>contents wrapped</Box>
+            </Skeleton>
+         </VStack>   
+        ))}
+
+        {!isLoading && (
+            <>
+                <FeedPost img='/img1.png' username='ye' avatar='/img1.png' />
+                <FeedPost img='/img2.png' username='bobby' avatar='/img2.png' />
+                <FeedPost img='/img3.png' username='kelly' avatar='/img3.png' />
+                <FeedPost img='/img4.png' username='johndoe' avatar='/img4.png' />
+            </>
+        )}
     </Container>
   )
 }
