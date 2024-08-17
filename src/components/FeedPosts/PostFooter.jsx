eec4b3d1c0969295275acd5c14f4bc3043d6074c
Danyal Imran <dan.imran97@gmail.com>
2024-08-17 14:26:03 -0400

    feed post stuff is done

--- src/components/FeedPosts/PostFooter.jsx
+++ src/components/FeedPosts/PostFooter.jsx
@@ -1,6 +1,6 @@
-import { Flex, Box } from "@chakra-ui/react"
+import { Flex, Box, Text, Input, InputGroup, InputRightElement, Button } from "@chakra-ui/react"
 import { useState } from "react"
-import { NotificationsLogo, UnlikeLogo} from '../../assets/constants'
+import { CommentLogo, NotificationsLogo, UnlikeLogo} from '../../assets/constants'
 
 
 const PostFooter = () => {
@@ -19,13 +19,42 @@ const PostFooter = () => {
 
 
   return (
-    <>
-      <Flex alignItems={"center"} gap={4} w={"full"} pt={0} mb={2} mt={"auto"}>
+    <Box mb={10}>
+      <Flex alignItems={"center"} gap={4} w={"full"} pt={0} mb={2} mt={4}>
         <Box onClick={handleLike} cursor={"pointer"} fontSize={18}>
           {!liked ? (<NotificationsLogo />) : (<UnlikeLogo /> )}
         </Box>
+
+        <Box cursor={"pointer"} fontSize={18}>
+          <CommentLogo />
+        </Box>
       </Flex>
-    </>
+      <Text fontWeight={600} fontSize={"sm"}>
+        {likes} likes
+      </Text>
+      <Text fontSize={"sm"} fontWeight={700}>
+        danimran {" "}
+        <Text as="span" fontWeight={400}>
+          Feeling Solid
+        </Text>
+      </Text>
+      <Text fontSize="sm" color={"gray"}>
+        View all 1,000 comments
+      </Text>
+
+      <Flex alignItems={"center"} gap={2} justifyContent={"space-between"} w={"full"}>
+        <InputGroup>
+          <Input variant={"flushed"} placeholder={"Add a comment..."} fontSize={14} />
+            <InputRightElement>
+              <Button fontSize={14} color={"blue.500"} cursor={"pointer"} _hover={{color:"white"}} bg={"transparent"}>
+                Post
+              </Button>
+            </InputRightElement>
+        </InputGroup>
+      </Flex>
+
+
+    </Box>
   )
 }
 
