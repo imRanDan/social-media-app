d5df16dc7ec0459c5a342e6fe5a6e15ef798dcaa
Danyal Imran <dan.imran97@gmail.com>
2024-08-17 14:08:20 -0400

    added a like / unlike feature

--- src/components/FeedPosts/PostFooter.jsx
+++ src/components/FeedPosts/PostFooter.jsx
@@ -1,8 +1,31 @@
+import { Flex, Box } from "@chakra-ui/react"
+import { useState } from "react"
+import { NotificationsLogo, UnlikeLogo} from '../../assets/constants'
 
 
 const PostFooter = () => {
+  const [liked, setLiked] = useState(false);
+  const [likes, setLikes] = useState(1000);
+
+  const handleLike = () => {
+    if(liked) {
+      setLiked(false);
+      setLikes(likes - 1);
+    } else {
+      setLiked(true);
+      setLikes(likes + 1);
+    }
+  }
+
+
   return (
-    <div>PostFooter</div>
+    <>
+      <Flex alignItems={"center"} gap={4} w={"full"} pt={0} mb={2} mt={"auto"}>
+        <Box onClick={handleLike} cursor={"pointer"} fontSize={18}>
+          {!liked ? (<NotificationsLogo />) : (<UnlikeLogo /> )}
+        </Box>
+      </Flex>
+    </>
   )
 }
 
