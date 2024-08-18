9663ec2252b65da8587be7b12addd4a600d79b54
Danyal Imran <dan.imran97@gmail.com>
2024-08-18 13:01:11 -0400

    working on suggesteduser components

--- src/components/SuggestedUsers/SuggestedUsers.jsx
+++ src/components/SuggestedUsers/SuggestedUsers.jsx
@@ -1,11 +1,34 @@
-import { VStack } from "@chakra-ui/react"
+import { VStack, Text, Link, Box, Flex } from "@chakra-ui/react"
 import SuggestedHeader from "./SuggestedHeader"
-
+import SuggestedUser from "./SuggestedUser"
 
 const SuggestedUsers = () => {
   return (
     <VStack py={8} px={6} gap={4}>
         <SuggestedHeader />
+
+        <Flex alignItems={"center"} justifyContent={"space-between"} w={"full"}>
+          <Text fontSize={12} fontWeight={"bold"} color={"gray.500"}>
+            Suggested for you
+          </Text>
+          <Text fontSize={12} fontWeight={"bold"} color={"gray.500"} _hover={{ color: "gray.400" }} cursor={"pointer"}>
+            See all
+          </Text>
+        </Flex>
+
+        <SuggestedUser />
+
+        <SuggestedUser />
+
+        <SuggestedUser />
+
+        <Box fontSize={12} color={"gray.500"} mt={5}>
+          2024 Built By {" "}
+          <Link href='https://github.com/imRanDan' target='_blank' color='blue.500' fontSize={14}>
+            imRanDan
+          </Link>
+        </Box>
+
     </VStack>
   )
 }
