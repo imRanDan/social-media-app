dba4f0f62d57f97d8efb4e54acdbc913aa2adbbf
Danyal Imran <dan.imran97@gmail.com>
2024-08-25 15:19:26 -0400

    finished custom hook to sign user in and out and removes from localstorage

--- src/components/SuggestedUsers/SuggestedHeader.jsx
+++ src/components/SuggestedUsers/SuggestedHeader.jsx
@@ -1,19 +1,29 @@
-import { Avatar, Flex, Link, Text } from "@chakra-ui/react"
-import { Link as RouterLink } from "react-router-dom"
+import { Avatar, Button, Flex, Text } from "@chakra-ui/react"
+
+import useLogout from "../../hooks/useLogout"
+import useAuthStore from "../../store/authStore"
+import { Link } from "react-router-dom"
 
 
 const SuggestedHeader = () => {
+  const {handleLogout, isLoggingOut} = useLogout()
+  const authUser = useAuthStore(state => state.user);
+
   return (
     <Flex justifyContent={"space-between"} alignItems={"center"} w={"full"}>
         <Flex alignItems={"center"} gap={2}>
-            <Avatar name='Dan Imran' size={"lg"} src="/profilepic.jpg" />
-                <Text fontSize={12} fontWeight={"bold"}>
-                    danimran
-                </Text>
+          <Link to={`${authUser.username}`}>
+            <Avatar  size={"lg"} src={authUser.profilePicURL} />
+          </Link>
+          <Link to={`${authUser.username}`}>
+            <Text fontSize={12} fontWeight={"bold"}>
+              {authUser.username}
+            </Text>
+          </Link>
         </Flex>
-        <Link as={RouterLink} to={"/auth"} fontWeight={"medium"} color={"blue.400"} style={{ textDecoration: "none"}} cursor={"pointer"}>
+        <Button size={"xs"} background={"transparent"} _hover={{ background: "transparent"}} fontSize={14} fontWeight={"medium"} color={"blue.400"} style={{ textDecoration: "none"}} cursor={"pointer"} onClick={handleLogout} isLoading={isLoggingOut}>
             Log out
-        </Link>
+        </Button>
     </Flex>
   )
 }
