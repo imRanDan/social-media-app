162b57cb1281a1c90ade7f816a59f13353bda56e
Danyal Imran <dan.imran97@gmail.com>
2024-09-02 13:21:04 -0400

    working on use follow component

---
+++ src/hooks/useFollowUser.js
@@ -0,0 +1,35 @@
+import { auth } from "../firebase/firebase"
+import useAuthStore from "../store/authStore"
+import { useEffect, useState } from "react"
+
+
+const useFollowUser = (userId) => {
+    const [isLoading, setIsLoading] = useState(false)
+    const [isFollowing, setIsFollowing] = useState(false)
+    const {authUser, setAuthUser} = useAuthStore()
+    const {userProfile, setUserProfile} = useUserProfileStore()
+    const showToast = useShowToast()
+    
+
+    const handleFollowUser = async () => {
+        setIsLoading(true)
+        try {
+            
+        } catch (error) {
+         showToast("Error", error.message, "error")   
+        } finally {
+            setIsLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        if(authUser) {
+            const isFollowing = authUser.following.includes(userId)
+            setIsFollowing(isFollowing)
+        }
+    },[authUser, userId])
+
+    return {isLoading, isFollowing, handleFollowUser}
+}
+
+export default useFollowUser
