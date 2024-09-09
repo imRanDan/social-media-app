a30211146d24d7b7775d3b67bf2bb3c216f8f0c3
Danyal Imran <dan.imran97@gmail.com>
2024-09-09 15:07:05 -0400

    added create post functionality where user can write caption and choose/upload an image

---
+++ src/store/postStore.js
@@ -0,0 +1,12 @@
+import {create} from "zustand"
+
+const usePostStore = create((set) => ({
+    posts:[],
+    createPost: (post) => set(state => ({posts:[post, ...state.posts]}))
+    // deletePost
+    // addComment
+    // setPosts
+}))
+
+
+export default usePostStore
