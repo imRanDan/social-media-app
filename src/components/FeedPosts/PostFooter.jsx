8e4ed46a804871e8488900c4e6e02968f792f181
Danyal Imran <dan.imran97@gmail.com>
2024-09-10 10:16:29 -0400

    placeholder likes have been removed and users can like / unlike posts

--- src/components/FeedPosts/PostFooter.jsx
+++ src/components/FeedPosts/PostFooter.jsx
@@ -1,16 +1,18 @@
 import { Flex, Box, Text, Input, InputGroup, InputRightElement, Button } from "@chakra-ui/react"
-import { useState } from "react"
+import { useRef, useState } from "react"
 import { CommentLogo, NotificationsLogo, UnlikeLogo} from '../../assets/constants'
 import usePostComment from "../../hooks/usePostComment";
 import useAuthStore from "../../store/authStore";
+import useLikePost from "../../hooks/useLikePost";
 
 
 const PostFooter = ({ post, username, isProfilePage}) => {
-  const [liked, setLiked] = useState(false);
-  const [likes, setLikes] = useState(1000);
+
   const {isCommenting, handlePostComment} = usePostComment()
   const [comment, setComment] = useState("")
   const authUser = useAuthStore(state => state.user)
+  const commentRef = useRef(null)
+  const {handleLikePost, isLiked, likes} = useLikePost(post)
 
 
 
@@ -19,25 +21,15 @@ const PostFooter = ({ post, username, isProfilePage}) => {
     setComment("")
   }
 
-  const handleLike = () => {
-    if(liked) {
-      setLiked(false);
-      setLikes(likes - 1);
-    } else {
-      setLiked(true);
-      setLikes(likes + 1);
-    }
-  }
-
 
   return (
     <Box mb={10} marginTop={"auto"}>
       <Flex alignItems={"center"} gap={4} w={"full"} pt={0} mb={2} mt={4}>
-        <Box onClick={handleLike} cursor={"pointer"} fontSize={18}>
-          {!liked ? (<NotificationsLogo />) : (<UnlikeLogo /> )}
+        <Box onClick={handleLikePost} cursor={"pointer"} fontSize={18}>
+          {!isLiked ? (<NotificationsLogo />) : (<UnlikeLogo /> )}
         </Box>
 
-        <Box cursor={"pointer"} fontSize={18}>
+        <Box cursor={"pointer"} fontSize={18} onClick={() => commentRef.current.focus()}>
           <CommentLogo />
         </Box>
       </Flex>
@@ -63,7 +55,7 @@ const PostFooter = ({ post, username, isProfilePage}) => {
       {authUser&& (
               <Flex alignItems={"center"} gap={2} justifyContent={"space-between"} w={"full"}>
               <InputGroup>
-                <Input variant={"flushed"} placeholder={"Add a comment..."} fontSize={14} onChange={(e) => setComment(e.target.value)} value={comment}/>
+                <Input variant={"flushed"} placeholder={"Add a comment..."} fontSize={14} onChange={(e) => setComment(e.target.value)} value={comment} ref={commentRef}/>
                   <InputRightElement>
                     <Button fontSize={14} color={"blue.500"} cursor={"pointer"} _hover={{color:"white"}} bg={"transparent"} onClick={handleSubmitComment} isLoading={isCommenting}>
                       Post
