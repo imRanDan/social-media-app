942eedcabca005a57800534f1b12f22c3228526d
Danyal Imran <dan.imran97@gmail.com>
2024-09-10 18:06:12 -0400

    added clickable link to profile and fixed creatorProfile error

--- src/components/FeedPosts/PostHeader.jsx
+++ src/components/FeedPosts/PostHeader.jsx
@@ -1,14 +1,22 @@
 import { Flex, Avatar, Box, Text } from "@chakra-ui/react"
+import { Link } from "react-router-dom"
 
 const PostHeader = ({post, creatorProfile}) => {
 
+  if (!creatorProfile) {
+    return null
+  }
 
   return (
     <Flex justifyContent={"space-between"} alignItems={"center"} w={"full"} my={2}>
       <Flex alignItems={"center"} gap={2}>
-        <Avatar src={creatorProfile.profilePicURL} alt={"profile pic of user"} size={"sm"}/>
+        <Link to={`${creatorProfile.username}`}>
+          <Avatar src={creatorProfile.profilePicURL} alt={"profile pic of user"} size={"sm"}/>
+        </Link>
         <Flex fontSize={12} fontWeight={"bold"} gap={2}>
-          {creatorProfile.username}
+          <Link to={`/${creatorProfile.username}`}>
+            {creatorProfile.username}
+          </Link>
           <Box color={"gray.500"}>1w</Box>
         </Flex>
       </Flex>
